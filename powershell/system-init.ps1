af77a4eabefd4a29ea0eb7a51efec7c13024c582
Scott Barning <ssbarning@gmail.com>
2023-12-09 01:12:38 -0500

    simplifying my life

--- powershell/system-init.ps1
+++ powershell/system-init.ps1
@@ -1,5 +1,4 @@
 $neovimConfigDir = "C:\Users\$env:USERPROFILE\nvim\"
-$gitDir = "C:\git\neovim"
 $patternToSkip = ".git*"
 
 
@@ -14,32 +13,7 @@ function Install-NvimConfig {
     
     Write-Host "Cloning init.lua repository..."
 
-    git clone https://github.com/BladeWDR/init.lua.git $gitDir
-
-    if (Test-Path -Path $gitDir -PathType Container) {
-        # Create the destination folder if it doesn't exist
-        if (-not (Test-Path -Path $neovimConfigDir -PathType Container)) {
-            New-Item -ItemType Directory -Path $neovimConfigDir | Out-Null
-        }
-
-        # Get all files and folders in the source folder
-        $items = Get-ChildItem -Path $gitDir
-
-        # Iterate through each file and folder
-        foreach ($item in $items) {
-            if ($item.Name -notlike $patternToSkip) {
-            # Create the destination path for the file or folder
-            $destinationPath = Join-Path -Path $neovimConfigDir -ChildPath $item.Name
-
-            # Copy the file or folder to the destination
-            New-Item -ItemType SymbolicLink -Path $destinationPath -Target $item.FullName
-            }
-        }
-
-        Write-Host "Files and folders copied successfully."
-    } else {
-        Write-Host "Source folder does not exist."
-    }
+    git clone https://github.com/BladeWDR/init.lua.git $neovimConfigDir
 
 
 
