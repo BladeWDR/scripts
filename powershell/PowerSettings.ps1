6d33858d73e5c6cc0df7be50509a8008a021689f
BladeWDR <ssbarning@gmail.com>
2024-01-03 13:22:33 -0500

    working on a function to create the plan if it doesn't exist

--- powershell/PowerSettings.ps1
+++ powershell/PowerSettings.ps1
@@ -16,6 +16,43 @@ function powerPlanConfiguration {
     Write-Host "High Performance Plan not found on this computer."
 }
 
+# function createPowerPlan {
+#
+#     #define the name of our new scheme
+#     $powerSchemeName = "MDT High Performance"
+#
+#     #Get the GUID of the active scheme.
+#     $activeScheme = $(powercfg -getactivescheme).split()[3]
+#
+#     #output the names of all schemes.
+#     $schemes = powercfg /list | Out-String -Stream
+#
+#     $mdtScheme = $schemes | Where-Object { $_ -match $powerSchemeName }
+#
+#     $PowerGUID = '4f971e89-eebd-4455-a8de-9e59040e7347'
+#     $PowerButtonGUID = '7648efa3-dd9c-4e3e-b566-50f929386280'
+#     $LidClosedGUID = '5ca83367-6e45-459f-a27b-476b1d01c936'
+#     $SleepGUID = '238c9fa8-0aad-41ed-83f4-97be242c8f20'
+#
+#     if ($null -eq $mdtScheme){
+#
+#         Write-Host "Power scheme '$mdtScheme' not found. Adding..."
+#
+#         powercfg /duplicatescheme $activeScheme
+#         powercfg -setactive ($powerSchemeName).split[3]
+#         #Set the power button to shut down the machine when pressed.
+#         powercfg /setdcvalueindex $powerSchemeName $PowerGUID $PowerButtonGUID 3
+#         #Set the computer to never sleep automatically.
+#         #Set the computer screen timeout.
+#         # add a conditional here to set the lid close action if the device is a laptop. 
+#
+#     }
+#     else {
+#         Write-Host "Power scheme '$powerSchemeName' already exists."
+#     }
+#
+# }
+
 #calls the above function.
 powerPlanConfiguration
 
@@ -28,3 +65,4 @@ powercfg -Change standby-timeout-ac 0
 
 
     }
+
