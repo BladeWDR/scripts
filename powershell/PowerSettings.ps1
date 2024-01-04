95b55c69efcae90b697769e22085f8975f0d975a
BladeWDR <ssbarning@gmail.com>
2024-01-03 19:46:41 -0500

    finishing Create-PowerPlan function.

--- powershell/PowerSettings.ps1
+++ powershell/PowerSettings.ps1
@@ -1,68 +1,89 @@
 #This is a script I use in my MDT deployments as a part of a task sequence.
 #function to set the power plan in Windows to high performance.
 
-function powerPlanConfiguration {
-
-    #first we need to grab the GUID of the high performance plan since it may vary across systems.
-    $powerPlans = powercfg /list
-    $highPerformancePlan = $powerPlans | Select-String "High Performance"
-
-    if ($highPerformancePlan) {
-    $guid = ($highPerformancePlan -split ' ')[3].Trim()
-    Write-Host "GUID of High Performance Plan: $guid"
-    Write-Host "Setting power plan to high performance."
-    powercfg -setactive $guid
-}   else {
-    Write-Host "High Performance Plan not found on this computer."
-}
-
-# function createPowerPlan {
+# function powerPlanConfiguration {
 #
-#     #define the name of our new scheme
-#     $powerSchemeName = "MDT High Performance"
+#     #first we need to grab the GUID of the high performance plan since it may vary across systems.
+#     $powerPlans = powercfg /list
+#     $highPerformancePlan = $powerPlans | Select-String "High Performance"
 #
-#     #Get the GUID of the active scheme.
-#     $activeScheme = $(powercfg -getactivescheme).split()[3]
-#
-#     #output the names of all schemes.
-#     $schemes = powercfg /list | Out-String -Stream
-#
-#     $mdtScheme = $schemes | Where-Object { $_ -match $powerSchemeName }
-#
-#     $PowerGUID = '4f971e89-eebd-4455-a8de-9e59040e7347'
-#     $PowerButtonGUID = '7648efa3-dd9c-4e3e-b566-50f929386280'
-#     $LidClosedGUID = '5ca83367-6e45-459f-a27b-476b1d01c936'
-#     $SleepGUID = '238c9fa8-0aad-41ed-83f4-97be242c8f20'
-#
-#     if ($null -eq $mdtScheme){
-#
-#         Write-Host "Power scheme '$mdtScheme' not found. Adding..."
-#
-#         powercfg /duplicatescheme $activeScheme
-#         powercfg -setactive ($powerSchemeName).split[3]
-#         #Set the power button to shut down the machine when pressed.
-#         powercfg /setdcvalueindex $powerSchemeName $PowerGUID $PowerButtonGUID 3
-#         #Set the computer to never sleep automatically.
-#         #Set the computer screen timeout.
-#         # add a conditional here to set the lid close action if the device is a laptop. 
-#
-#     }
-#     else {
-#         Write-Host "Power scheme '$powerSchemeName' already exists."
-#     }
-#
+#     if ($highPerformancePlan) {
+#     $guid = ($highPerformancePlan -split ' ')[3].Trim()
+#     Write-Output "GUID of High Performance Plan: $guid"
+#     Write-Output "Setting power plan to high performance."
+#     powercfg -setactive $guid
+# }   else {
+#     Write-Output "High Performance Plan not found on this computer."
+# }
 # }
 
-#calls the above function.
-powerPlanConfiguration
+#checks to see if the device is a laptop and returns true if so.
+function Detect-Laptop
+{
+    Param( [string]$computer = 'localhost' )
+        $isLaptop = $false
+#The chassis is the physical container that houses the components of a computer. Check if the machine’s chasis type is 9.Laptop 10.Notebook 14.Sub-Notebook
+        if(Get-WmiObject -Class win32_systemenclosure -ComputerName $computer | Where-Object { $_.chassistypes -eq 9 -or $_.chassistypes -eq 10 -or $_.chassistypes -eq 14})
+        { 
+            $isLaptop = $true 
+        }
 
-#set the monitor timeout to a reasonable time and set the PC to never sleep.
-powercfg -Change monitor-timeout-ac 30
-powercfg -Change standby-timeout-ac 0
+
+    return $isLaptop
+}
 
 
+function Create-PowerPlan {
 
+    #define the name of our new scheme
+    $powerSchemeName = "MDT High Performance"
 
+    #Get the GUID of the active scheme.
+    $activeScheme = $(powercfg -getactivescheme).split()[3]
+
+    #output the names of all schemes.
+    $schemes = powercfg /list | Out-String -Stream
+
+    $mdtScheme = $schemes | Where-Object { $_ -match $powerSchemeName }
+
+    $PowerGUID = '4f971e89-eebd-4455-a8de-9e59040e7347'
+    $PowerButtonGUID = '7648efa3-dd9c-4e3e-b566-50f929386280'
+    $LidClosedGUID = '5ca83367-6e45-459f-a27b-476b1d01c936'
+    $SleepGUID = '238c9fa8-0aad-41ed-83f4-97be242c8f20'
+
+    if ($null -eq $mdtScheme){
+
+        Write-Output "Power scheme '$mdtScheme' not found. Adding..."
+
+        powercfg /duplicatescheme $activeScheme
+        powercfg -setactive ($powerSchemeName).split[3]
+        #Set the power button to shut down the machine when pressed.
+        powercfg /setdcvalueindex $powerSchemeName $PowerGUID $PowerButtonGUID 3
+        powercfg /setacvalueindex $powerSchemeName $PowerGUID $PowerButtonGUID 3
+        #Set the computer to never sleep automatically.
+        powercfg -change -standby-timeout-dc 0
+        #Set the computer screen timeout.
+        powercfg -change -monitor-timeout-ac 0
+        # add a conditional here to set the lid close action if the device is a laptop. 
+        if ( Detect-Laptop ){
+
+            powercfg /setdcvalueindex $powerSchemeName $PowerGUID $LidClosedGUID 15
+            powercfg /setacvalueindex $powerSchemeName $PowerGUID $LidClosedGUID 0
+
+        }
 
     }
+    else {
+        Write-Output "Power scheme '$powerSchemeName' already exists."
+    }
 
+}
+
+#calls the above function.
+
+try {
+    Create-PowerPlan
+}
+catch{
+        Write-Warning $psitem.Exception.Message
+    }
