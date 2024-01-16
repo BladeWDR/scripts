6308b38e09112ee9d7e844324059626dc28856bf
BladeWDR <ssbarning@gmail.com>
2024-01-15 20:18:55 -0500

    removing the edit-terminal function b/c M$ puts comments in their json

--- powershell/system-init.ps1
+++ powershell/system-init.ps1
@@ -11,23 +11,29 @@ function Install-Apps {
     winget install google.chrome --accept-package-agreements --accept-source-agreements
 }
 
-function Edit-Terminal {
-    $JsonPath = "$env:localappdata\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-
-# Check to see if the settings.json file exists.
-    if (Test-Path $JsonPath){
-        $fontProperty = @"
-{
-            "face": "CaskaydiaCove Nerd Font",
-            "size": 12.0
-        },
-"@
-	$JsonFile = Get-Content $JsonPath -raw
-	$JsonContent = ConvertFrom-Json -InputObject $JsonFile
-	$JsonContent.profiles.defaults | Add-Member -Name 'font' -Value (ConvertFrom-Json $fontProperty) -MemberType NoteProperty
-	$JsonContent | ConvertTo-Json -Depth 32 | Set-Content $JsonPath -Force
-}
-}
+# function Edit-Terminal {
+#     $JsonPath = "$env:localappdata\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+#
+# # Check to see if the settings.json file exists.
+#     if (Test-Path $JsonPath){
+#         $fontProperty = @"
+# {
+#             "face": "CaskaydiaCove Nerd Font",
+#             "size": 12.0
+#         },
+# "@
+# 	$JsonFile = Get-Content $JsonPath -raw
+#     if (-not $JsonFile -eq $null) {
+# 	$JsonContent = ConvertFrom-Json -InputObject $JsonFile
+# 	$JsonContent.profiles.defaults | Add-Member -Name 'font' -Value (ConvertFrom-Json $fontProperty) -MemberType NoteProperty
+# 	$JsonContent | ConvertTo-Json -Depth 32 | Set-Content $JsonPath -Force
+#     }
+#     else {
+#         "Json data is null."
+#     }
+# }
+# }
+#
 
 # Install chocolatey
 Install-WinUtilChoco
@@ -37,6 +43,6 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";"
 
 Install-Apps
 
-Edit-Terminal
+#Edit-Terminal
 
 Read-Host "Installs complete. Press Enter to continue..."
