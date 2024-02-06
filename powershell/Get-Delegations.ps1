08ddd30104506baaf9d06525890af81ad6c083a4
Scott <ssbarning@gmail.com>
2024-02-06 16:02:25 -0500

    Added Get-Delegations.ps1

---
+++ powershell/Get-Delegations.ps1
@@ -0,0 +1,54 @@
+###################################################
+# Get-Delegations.ps1 myadminuser@domain.onmicrosoft.com
+###################################################
+
+param(
+    [Parameter(Mandatory)][string]$defaultValue
+)
+$outputFile="$env:userprofile\Downloads\delegation.csv"
+
+if([string]::IsNullOrWhiteSpace($defaultValue))
+{
+    Read-Host "You must enter an account with admin rights."; EXIT
+}
+try {
+    Import-Module ExchangeOnlineManagement | Out-Null
+}
+catch {
+    Write-Host 'Installing Exchange Online module...'
+    Install-Module ExchangeOnlineManagement -AllowClobber
+    Import-Module ExchangeOnlineManagement | Out-Null
+    Clear-Host
+}
+finally {
+    Connect-ExchangeOnline -UserPrincipalName $defaultValue
+}
+# Get list of all mailboxes, shared and actual
+$mailboxPermissions = @()
+$delegates = @()
+
+# Get all mailboxes
+$mailboxes = Get-Mailbox -ResultSize Unlimited
+
+# Iterate over mailboxes and check for delegations
+foreach ($mailbox in $mailboxes) {
+    # Get mailbox delegates
+    write-host "Checking $mailbox"
+    $delegates = Get-MailboxPermission -Identity $mailbox.DistinguishedName  | Where-Object { $_.IsInherited -eq $false -and $_.User -ne "NT AUTHORITY\SELF" }
+# If delegates exist, add them to the array
+if ($delegates) {
+    foreach ($delegate in $delegates) {
+        $mailboxPermissions += [PSCustomObject]@{
+            "Mailbox" = $mailbox.UserPrincipalName
+                "Delegate" = $delegate.User
+                "Access Rights" = $delegate.AccessRights
+        }
+    }
+}
+}
+Clear-Host
+# export to csv
+$mailboxPermissions | Export-Csv -Path $outputFile -NoTypeInformation
+Write-Host "XXXXXXXXXXXXXXXXXXXXXXXX"
+Write-Host "Exported to $outputFile"
+Write-Host "XXXXXXXXXXXXXXXXXXXXXXXX"
