151351cc9c9324fbd2a0e09d5fac866a05be7bc1
Scott Barning <ssbarning@gmail.com>
2024-04-18 18:30:54 -0400

    cleaned up and updated

--- powershell/Custom-GetADMember.ps1
+++ powershell/Custom-GetADMember.ps1
@@ -1,17 +1,18 @@
+# Uses recursive function get all effective members of a group. (including nested groups)
+# Custom-GetADMember.ps1 -groupname whatevergroup
 param(
-    [Parameter(Mandatory)[string]$groupname]
+    [Parameter(Mandatory)][string]$groupname
 )
-if([string]::IsNullOrWhiteSpace($groupname){
+if([string]::IsNullOrWhiteSpace($groupname)){
     Read-Host "you must enter a group name!"; EXIT
     }
 
-
 function custom-getmembers {
-    $realmembers = @()
-
     param (
-        [string]$group
-    )
+            [string]$group
+          )
+
+    $realmembers = @()
 
     $groupmembers = Get-ADGroupMember -Identity $group
 
@@ -20,7 +21,7 @@ function custom-getmembers {
             custom-getmembers -group $member
         }
         else{
-            $realmembers += $member.name
+            $realmembers += $member
         }
     }
     return $realmembers
@@ -28,6 +29,4 @@ function custom-getmembers {
 
 $members = custom-getmembers -group $groupname | Sort-Object -Unique
 
-$memberList = $members | Select-Object @{Name='Name';Expression={$_}}
-$memberList | Export-Csv -path "$env:USERPROFILE\Downloads\$groupname.csv" -NoTypeInformation
-
+$members | Select-Object name | Export-Csv -path "$env:USERPROFILE\Downloads\$groupname.csv" -NoTypeInformation 
