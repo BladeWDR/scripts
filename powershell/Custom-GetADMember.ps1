84d0fcc419aa1b805eeab9ae78fc54d918480167
BladeWDR <ssbarning@gmail.com>
2024-04-18 16:21:00 -0400

    add Custom-GetADMember.ps1 (WIP)

--- powershell/Custom-GetADMember.ps1
+++ powershell/Custom-GetADMember.ps1
@@ -1,8 +1,14 @@
-$groupname = "Employee-Share-Publisher"
-$filePath = ""
-$realmembers = @()
+param(
+    [Parameter(Mandatory)[string]$groupname]
+)
+if([string]::IsNullOrWhiteSpace($groupname){
+    Read-Host "you must enter a group name!"; EXIT
+    }
+
 
 function custom-getmembers {
+    $realmembers = @()
+
     param (
         [string]$group
     )
@@ -23,5 +29,5 @@ function custom-getmembers {
 $members = custom-getmembers -group $groupname | Sort-Object -Unique
 
 $memberList = $members | Select-Object @{Name='Name';Expression={$_}}
-$memberList | Export-Csv -path $filePath -NoTypeInformation
+$memberList | Export-Csv -path "$env:USERPROFILE\Downloads\$groupname.csv" -NoTypeInformation
 
