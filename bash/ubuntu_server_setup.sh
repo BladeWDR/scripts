be87b7bff4a96806c936c434c2e97790a4964147
BladeWDR <scott@ssb-tech.net>
2024-06-28 18:05:06 -0400

    updated package list

--- bash/ubuntu_server_setup.sh
+++ bash/ubuntu_server_setup.sh
@@ -21,6 +21,7 @@ sudo apt update
 sudo apt install -y \
     anacron \
     apt-transport-https \
+    bat \
     bwm-ng \
     ca-certificates \
     curl \
@@ -40,7 +41,10 @@ sudo apt install -y \
     software-properties-common \
     vnstat \
     vim \
-    tmux
+    tmux \
+    zsh \
+    zsh-syntax-highlighting \
+    zsh-autosuggestions
 
 # speed up boot times
 # https://askubuntu.com/a/979493
