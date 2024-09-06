6fc2dc45b6b449eb77fdff198a32820bd585ca4b
BladeWDR <scott@ssb-tech.net>
2024-09-06 13:31:04 -0400

    update system-init script

--- powershell/system-init.ps1
+++ powershell/system-init.ps1
@@ -5,7 +5,9 @@ function Install-WinUtilChoco {
 
 function Install-Apps {
 # Install programs via choco
-    choco install 7zip adobereader firefox nerd-fonts-CascadiaCode winget
+    choco install 7zip adobereader firefox winget -y
+# Installs Open Shell without Classic Explorer.
+    choco install open-shell -y --install-arguments="'/qn ADDLOCAL=StartMenu'"
 # Use winget to install Chrome since the choco version is constantly broken.
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
     winget install google.chrome --accept-package-agreements --accept-source-agreements
