0ed337fc90df6ceb05c03c5a17b49112c6a9e17c
BladeWDR <ssbarning@gmail.com>
2025-01-08 15:53:54 -0500

    significant refactor

--- powershell/365/Remove-Groups.ps1
+++ powershell/365/Remove-Groups.ps1
@@ -1,13 +1,25 @@
-# Remove-Groups.ps1 John Smith
-# Remove-Groups.ps1 jsmith@domain.com
+<#
+  .SYNOPSIS
+  Removes user from all groups.
+
+  .DESCRIPTION
+  This script is intended to be used for offboarding.
+  It will remove a user from any groups or distribution lists that they are a part of.
+
+  .PARAMETER User
+  The user to be removed.
+
+  .EXAMPLE
+  PS> .\Remove-FromGroups.ps1 -User bob@example.com
+#>
 
 param(
-[Parameter(Mandatory)][string]$name
+    [Parameter(Mandatory=$true,
+    HelpMessage='Enter a user to be removed from all groups in an email address format.',
+    ValueFromPipeline=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$User
 )
-if([string]::IsNullOrWhiteSpace($name))
-{
-    Read-Host "Enter the user's name or email address"; EXIT
-}
 
 try {
     Import-Module ExchangeOnlineManagement
@@ -18,30 +30,65 @@ catch {
     Write-Host "Installed successfully."
 }
 
-$user = Get-Mailbox -Identity $name | Select-Object -ExpandProperty PrimarySmtpAddress
-$distributionLists = Get-DistributionGroup | Select-Object -ExpandProperty Name
-$office365Groups = Get-UnifiedGroup | Select-Object -ExpandProperty Name
+Write-Output "Removing user $User from all distribution lists and 365 groups."
+Write-Output "Stand by..."
+
+$distributionLists = Get-DistributionGroup | Select-Object DistinguishedName,DisplayName
+$office365Groups = Get-UnifiedGroup | Select-Object Name,DisplayName
+
+[boolean]$userFound = $false
 
 foreach ($list in $distributionLists) {
-    $distlistmembers = Get-DistributionGroupMember -Identity $list | Select-Object -ExpandProperty PrimarySmtpAddress
+
+    $listDN = $list.DistinguishedName
+    $listDisplayName = $list.DisplayName
+
+    $distlistmembers = Get-DistributionGroupMember -Identity $listDN | Select-Object -ExpandProperty PrimarySmtpAddress
     
-        if( $user -in $distlistmembers) {
-            Remove-DistributionGroupMember -Identity $list -Member $user -Confirm:$false
-            Write-Host "The user $user has been removed from the distribution list $list"
+        if( $User -in $distlistmembers) {
+            Remove-DistributionGroupMember -Identity $listDN -Member $User -Confirm:$false -Whatif
+            Write-Output "The user $User has been removed from the distribution list `"$listDisplayName`""
+            $userFound = $true
+        }
+        else{
+            Write-Verbose "User not found in distribution list `"$listDisplayName`"."
         }
 }
 
+if (-not ($userFound)){
+    Write-Output "User not found in any distribution lists."
+}
+
+[boolean]$userFound = $false
 
 foreach ($group in $office365Groups) {
-    $o365groupmembers = Get-UnifiedGroupLinks -Identity $group -LinkType Members | Select-Object -ExpandProperty PrimarySmtpAddress
-        if( $user -in $o365groupmembers) {
-            try {
-                Remove-UnifiedGroupLinks -Identity $group -LinkType Owners -Links $user -Confirm:$false
-            }
-            catch {
-                Write-Host "Error removing user as an owner of $group. It's likely that they're the only owner of the group."
-            }
-            Remove-UnifiedGroupLinks -Identity $group -LinkType Members -Links $user -Confirm:$false
-            Write-Host "The user $($user) has been removed from the Office 365 group $group"
-    }
+
+        $groupName = $group.Name
+        $groupDisplayName = $group.DisplayName
+        $o365groupmembers = Get-UnifiedGroupLinks -Identity $groupName -LinkType Members | Select-Object -ExpandProperty PrimarySmtpAddress
+
+        if( $User -in $o365groupmembers) {
+            $userFound = $true
+            $groupOwners = Get-UnifiedGroupLinks -Identity "$groupName" -LinkType Owners | Select-Object -ExpandProperty PrimarySmtpAddress
+            $groupName
+                if ( $User -in $groupOwners){
+                    try {
+                        Write-Output "Attempting to remove group ownership for `"$groupDisplayName`"..."
+                        Remove-UnifiedGroupLinks -Identity $groupName -LinkType Owners -Links $User -Confirm:$false -WhatIf
+                        Write-Output "Removed ownership from `"$groupDisplayName`" successfully."
+                    }
+                    catch {
+                        Write-Host "Error removing user as an owner of $groupName. It's likely that they're the only owner of the group."
+                    }
+                }
+            Remove-UnifiedGroupLinks -Identity $groupName -LinkType Members -Links $User -Confirm:$false -WhatIf
+                Write-Output "The user $($User) has been removed from the Office 365 group `"$groupDisplayName`""
+        }
+        else{
+            Write-Verbose "User not found in Office 365 group `"$groupDisplayName`"."
+        }
+}
+
+if (-not ($userFound)){
+    Write-Output "User not found in any Office 365 groups."
 }
