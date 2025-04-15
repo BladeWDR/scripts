71c321ee83803ec4822b6240b7a80bd728641931
BladeWDR <scott@ssb-tech.net>
2025-04-15 09:27:11 -0400

    removed unused function

--- powershell/system-init.ps1
+++ powershell/system-init.ps1
@@ -1,38 +1,16 @@
-function Install-WinUtilChoco {
-        Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -ErrorAction Stop
-        powershell choco feature enable -n allowGlobalConfirmation
+function Install-WinUtilChoco
+{
+    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -ErrorAction Stop
+    powershell choco feature enable -n allowGlobalConfirmation
 }
 
-function Install-Apps {
-# Install programs via choco
+function Install-Apps
+{
+    # Install programs via choco
     choco install 7zip firefox -y
-# Installs Open Shell without Classic Explorer.
+    # Installs Open Shell without Classic Explorer.
     choco install open-shell -y --install-arguments="'/qn ADDLOCAL=StartMenu'"
 }
-
-# function Edit-Terminal {
-#     $JsonPath = "$env:localappdata\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-#
-# # Check to see if the settings.json file exists.
-#     if (Test-Path $JsonPath){
-#         $fontProperty = @"
-# {
-#             "face": "CaskaydiaCove Nerd Font",
-#             "size": 12.0
-#         },
-# "@
-# 	$JsonFile = Get-Content $JsonPath -raw
-#     if (-not $JsonFile -eq $null) {
-# 	$JsonContent = ConvertFrom-Json -InputObject $JsonFile
-# 	$JsonContent.profiles.defaults | Add-Member -Name 'font' -Value (ConvertFrom-Json $fontProperty) -MemberType NoteProperty
-# 	$JsonContent | ConvertTo-Json -Depth 32 | Set-Content $JsonPath -Force
-#     }
-#     else {
-#         "Json data is null."
-#     }
-# }
-# }
-#
 
 # Install chocolatey
 Install-WinUtilChoco
